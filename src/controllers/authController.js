6ae498900cd8cba5cb3dc5ca2479aa24649c97e4
Pratap Navale <navalepratap2005@outlook.com>
2026-06-13 21:39:22 +0530

    Implemented JWT authentication system

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const prisma = require("../prisma/prismaClient");
 
+// REGISTER
 const register = async (req, res) => {
   try {
-
     const { name, email, password } = req.body;
 
     const existingUser = await prisma.user.findUnique({
@@ -26,27 +27,79 @@ const register = async (req, res) => {
       }
     });
 
-   res.status(201).json({
-    message: "User registered successfully",
-    user: {
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
         id: user.id,
         name: user.name,
         email: user.email,
         role: user.role
-    }
-});
+      }
+    });
 
   } catch (error) {
-
     console.error(error);
 
     res.status(500).json({
       message: "Server Error"
     });
+  }
+};
 
+// LOGIN
+const login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: user.role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server Error"
+    });
   }
 };
 
 module.exports = {
-  register
+  register,
+  login
 };
